efb04cec766922701cf693458b23afcb1116c861
Acolonia <77890055+Acolonia@users.noreply.github.com>
2021-02-26 20:35:46 -0500

    Hw Assignment

---
+++ Function.js
@@ -0,0 +1,27 @@
+Function
+function storeAgenumber(Agenumber){
+    sessionStorage.setItem('Agenumber', Agenumber);
+}
+
+function getAgenumber(){
+   return sessionStorage.getItem('Agenumber');
+}
+
+const myAgenumber = 14;
+
+storeAgenumber(myAgenumber);
+
+getAgenumber();
+
+const School = {
+    schoolName: 'Jose Marti Stem Academy',
+    city: 'Union City',
+    state: 'NJ',
+    country: 'United States',
+    age: 2,
+    getAddress: function() {
+        return this.city + ', ' + this.state + ', ' + this.country 
+    }
+};
+
+School.getAddress(); 
